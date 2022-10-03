82bbf3428455d93dded8cbe45e8936e24a530d66
Joel-D4Ni31 <018201540d@uandina.edu.pe>
2022-10-03 17:58:14 -0500

    tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Cliente } from './model/product.model';
 import { Product } from './model/product.model';
-
+import { Sale } from './model/product.model';
+import { DetailSale } from './model/product.model';
+import { Venta } from './model/product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -35,12 +38,58 @@ export class AppComponent {
   p_name: string = "";
   p_price: number = 0;
   p_image: string = "";
+  p_description: string ="";
+  p_porcentaje: string = "";
+  p_estado: string ="";
   products: Product[] = [{
     name: "parquet",
     price: 28,
-    image: "https://media.istockphoto.com/photos/sample-of-wood-chipboard-wooden-laminate-veneer-material-for-interior-picture-id1135678299?b=1&k=20&m=1135678299&s=612x612&w=0&h=JVmRpRls_0Pq9f5GT2EPtmWXbzlLMXGlKX6L4YOuKU8="
+    image: "https://media.istockphoto.com/photos/sample-of-wood-chipboard-wooden-laminate-veneer-material-for-interior-picture-id1135678299?b=1&k=20&m=1135678299&s=612x612&w=0&h=JVmRpRls_0Pq9f5GT2EPtmWXbzlLMXGlKX6L4YOuKU8=",
+    description: "Paquete de Parquet  x24 unidades",
+    porcentaje: "100%",
+    estado: "Entregado"
   }];
+  clients: Cliente[] = [{
+    name: "Doraemon",
+    direccion: "AV LA CULTURA ' SIN Nº",
+    correo: "Dora3mon@demon.mon"
+  }];
+  sales: Sale[] = [{
+    fechaEntrega: "15/10/22",
+    fechaVenta: "03/10/22",
+    priceTotal: 140,
+    codCLiente: "Doraemon"
+  }];
+
+  d_descripcion: string ="";
+  d_ruta: string ="";
+  d_nombre: string ="";
+  d_porcentaje: string ="";
+  d_estado: string ="";
+
+  detailsales: DetailSale[] = [{
+    cantidad: 5,
+    precio: 28,
+    subtotal: 140,
+    codVenta: "Doraemon",
+    codProducto: "parquet"
+  }];
+  ventas: Venta[] = [{
+    name: "parquet",
+    image: "https://media.istockphoto.com/photos/sample-of-wood-chipboard-wooden-laminate-veneer-material-for-interior-picture-id1135678299?b=1&k=20&m=1135678299&s=612x612&w=0&h=JVmRpRls_0Pq9f5GT2EPtmWXbzlLMXGlKX6L4YOuKU8=",
+    description: "Paquete de Parquet  x24 unidades",
+    porcentaje: "100%",
+    estado: "Entregado"
+  }];
+ 
   
+  verComprobante(): void{
+    this.p_name="";
+  }
+  actComprobante(position: number): void{
+    this.persons.splice(position);
+  }
+
   cambioParametro(): void{
     this.btn = !this.btn;
   }
@@ -78,12 +127,17 @@ export class AppComponent {
     const newProduct ={
       name: this.p_name,
       price: this.p_price,
-      image: this.p_image
+      image: this.p_image,
+      description: this.p_description,
+      porcentaje: this.p_porcentaje,
+      estado: this.p_estado
     }
     this.products.push(newProduct);
     this.p_name = "";
     this.p_price = 0;
     this.p_image = "";
+    this.p_porcentaje="";
+    this.p_estado="";
   }
 }
 
--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -1,5 +1,35 @@
+export interface Cliente{
+    name: string;
+    direccion: string;
+    correo: string;
+}
 export interface Product{
     name: string;
     price: number;
     image: string;
+    description: string;
+    porcentaje: string;
+    estado: string;
+}
+export interface Sale{
+    fechaEntrega: string;
+    fechaVenta: string;
+    priceTotal: number;
+    codCLiente: string;
+}
+export interface DetailSale{
+    cantidad: number;
+    precio: number;
+    subtotal: number;
+    codVenta: string;
+    codProducto: string;
+}
+
+export interface Venta{
+    name: string;
+    image: string;
+    description: string;
+    porcentaje: string;
+    estado: string;
+
 }
